e49b2268a2a4de1a695cfc4f1427bbac612845a1
AineBoyle <ainegboyle@hotmail.com>
2017-01-25 15:17:01 +0000

    Working with Adapt training set

--- word2vec.py
+++ word2vec.py
@@ -1,29 +1,26 @@
-import pyodbc
-
-import pandas as pd
 import gensim
+import pyodbc
+import pandas as pd
 
-train = pd.read_csv("labeledTrainData.tsv", header = 0, delimiter = "\t", quoting = 3)
-print("Read labeled train reviews, ", (train["review"].size))
+#Train model w/ Training Dataset
+train = pd.read_csv("traintweets.csv", names = ['id', 'tweet', 'sentiment'])
+print("Read labeled train reviews, ", (train["tweet"].size))
 
-#con = pyodbc.connect(Trusted_Connection='yes', driver = '{SQL Server}',server = 'GANESHA\SQLEXPRESS' , database = '4YP')
-#print("Connected")
+#Get Tweets from DB
+con = pyodbc.connect(Trusted_Connection='yes', driver = '{SQL Server}',server = 'GANESHA\SQLEXPRESS' , database = '4YP')
+print("Connected")
 
-#cur = con.cursor()
+cur = con.cursor()
 
-#cur.execute("SELECT * FROM twitter_data")
+cur.execute("SELECT * FROM twitter_data")
 
-#tweets = []
-#for row in cur.fetchall():
-#    tweets.append(row[3])
-#    model = gensim.models.Word2Vec(row[3], min_count = 1)
-#    print("model: " , model)
-#con.close()
-#model = gensim.models.Word2Vec(sentences, size=100, window=5, min_count=5, workers=4)
-#model = gensim.models.Word2Vec.load("C:/Users/Aine/Documents/College/Y4/Final Year Project/annotated_train_tweets.csv")
-#model['budget']
-#for tweet in tweets:
-#    print(tweet)
+tweets = []
+for row in cur.fetchall():
+    tweets.append(row[3])
+    model = gensim.models.Word2Vec(row[3], min_count = 1)
+    print("model: " , model)
+con.close()
+
 #sentences = [['first', 'sentence'], ['second', 'sentence']]
 #model = gensim.models.Word2Vec(sentences, min_count=1)
 #print("model: ", model)

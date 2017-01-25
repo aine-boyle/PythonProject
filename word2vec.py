79ec751bc430ca129e30135fff71fdf5d02e3dab
AineBoyle <ainegboyle@hotmail.com>
2017-01-25 14:47:00 +0000

    training set read in

--- word2vec.py
+++ word2vec.py
@@ -1,22 +1,27 @@
 import pyodbc
 
+import pandas as pd
 import gensim
 
-con = pyodbc.connect(Trusted_Connection='yes', driver = '{SQL Server}',server = 'GANESHA\SQLEXPRESS' , database = '4YP')
-print("Connected")
+train = pd.read_csv("labeledTrainData.tsv", header = 0, delimiter = "\t", quoting = 3)
+print("Read labeled train reviews, ", (train["review"].size))
 
-cur = con.cursor()
+#con = pyodbc.connect(Trusted_Connection='yes', driver = '{SQL Server}',server = 'GANESHA\SQLEXPRESS' , database = '4YP')
+#print("Connected")
 
-# Use all the SQL you like
-cur.execute("SELECT * FROM twitter_data")
+#cur = con.cursor()
 
-# print all the first cell of all the rows
-tweets = []
-for row in cur.fetchall():
-    tweets.append(row[3])
-    model = gensim.models.Word2Vec(row[3], min_count = 1)
-    print("model: " , model)
-con.close()
+#cur.execute("SELECT * FROM twitter_data")
+
+#tweets = []
+#for row in cur.fetchall():
+#    tweets.append(row[3])
+#    model = gensim.models.Word2Vec(row[3], min_count = 1)
+#    print("model: " , model)
+#con.close()
+#model = gensim.models.Word2Vec(sentences, size=100, window=5, min_count=5, workers=4)
+#model = gensim.models.Word2Vec.load("C:/Users/Aine/Documents/College/Y4/Final Year Project/annotated_train_tweets.csv")
+#model['budget']
 #for tweet in tweets:
 #    print(tweet)
 #sentences = [['first', 'sentence'], ['second', 'sentence']]

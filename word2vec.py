2c930e831d794dab2e9422a094ca87e70c675c25
AineBoyle <ainegboyle@hotmail.com>
2017-02-13 16:40:36 +0000

    Word2Vec with Review Dataset

--- word2vec.py
+++ word2vec.py
@@ -1,31 +1,42 @@
-from gensim.models import word2vec
 import pyodbc
-import pandas as pd
 import re
+import numpy as np
+
+from gensim.models import word2vec
+import pandas as pd
 from bs4 import BeautifulSoup
 import nltk.data
 nltk.download()
+from nltk.corpus import stopwords
 import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
                     level=logging.INFO)
 
 #Set Parameters
-num_features = 300  #Word vector dimensionality
+num_features = 300      #Word vector dimensionality
 min_word_count = 10
-num_workers = 4     #Number of threads to run in parallel
+num_workers = 4         #Number of threads to run in parallel
 context = 10
-downsampling = 1e-3 #Downsample setting for frequent words
+downsampling = 1e-3     #Downsample setting for frequent words
 
 #Train model w/ Training Dataset
-train = pd.read_csv("traintweets.csv", names = ['id', 'tweet', 'sentiment'])
-print("Read labeled train reviews, ", (train["tweet"].size))
+print("Reading CSV...")
+#train = pd.read_csv("traintweets.csv", names = ['id', 'tweet', 'sentiment'])
+train = pd.read_csv( "labeledTrainData.tsv", header=0,
+ delimiter="\t", quoting=3 )
+#train = pd.read_csv("labeledTrainData.tsv", names = ['id', 'sentiment', 'review'])
+print("Read labeled train reviews, ", (train["review"].size))
 
 #Clean Tweets
-def tweet_to_wordlist(tweet) :
+def tweet_to_wordlist(tweet, remove_stopwords=False) :
     tweet_text = BeautifulSoup(tweet, "lxml").get_text()
     #tweet_text = re.sub("[^a-zA-Z]","",tweet_text)
     words = tweet_text.lower().split()
+
+    if remove_stopwords:
+        stops = set(stopwords.words("english"))
+        words = [w for w in words if not w in stops]
 
     return(words)
 
@@ -42,7 +53,7 @@ def tweet_to_sentences (tweet, tokenizer):
 
 sentences = []
 print("Parsing sentences from training set")
-for tweet in train["tweet"]:
+for tweet in train["review"]:
     sentences += tweet_to_sentences(tweet, tokenizer)
 
 print(len(sentences))
@@ -60,7 +71,7 @@ model_name = "MyModel"
 model.save(model_name)
 
 print(model.doesnt_match("man woman child kitchen".split()))
-#Get Tweets from DB
+    #Get Tweets from DB
 #con = pyodbc.connect(Trusted_Connection='yes', driver = '{SQL Server}',server = 'GANESHA\SQLEXPRESS' , database = '4YP')
 #print("Connected")
 
